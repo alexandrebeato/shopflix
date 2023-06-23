3648601e287e424df497befa8890c965e2e796ab
ArthurGalanti <arthur.galanti@hotmail.com>
2023-06-23 14:54:09 -0300

    fix: status code form UserController

--- server/src/API/Controllers/UserController.cs
+++ server/src/API/Controllers/UserController.cs
@@ -2,7 +2,6 @@ using API.Models.Users;
 using API.Services;
 using AutoMapper;
 using Domain.Commands;
-using Domain.Commands.Contracts;
 using Domain.Handlers;
 using Domain.Repositories;
 using Domain.Users;
@@ -16,75 +15,81 @@ namespace API.Controllers
     {
         [HttpPost]
         [Route("register")]
-        public ICommandResult Register(
+        public IActionResult Register(
             [FromBody] RegisterUserCommand command,
             [FromServices]UserHandler handler,
             [FromServices]TokenService tokenService,
             [FromServices]IMapper mapper)
         {
             if (!ModelState.IsValid)
-                return new GenericCommandResult(false,
+                return BadRequest(new GenericCommandResult(false,
                     "Oops, looks like that lack anything!",
                     null,
-                    ModelState.Select(x => 
-                        new{x.Key, message = x.Value!.Errors[0].ErrorMessage}));
+                    ModelState.Select(x =>
+                        new { x.Key, message = x.Value!.Errors[0].ErrorMessage })));
 
             command.Validate();
             if (!command.IsValid)
-                return new GenericCommandResult(false,
+                return BadRequest(new GenericCommandResult(false,
                     "Oops, looks like anything is incorrect!",
                     null,
-                    command.Notifications.Select(x => new {x.Key, x.Message}));
+                    command.Notifications.Select(x => new {x.Key, x.Message})));
             
             command.Password = PasswordHasher.Hash(command.Password);
 
-             var result = handler.Handle(command);
+            try
+            {
+                var result = handler.Handle(command);
              
-             if (!result.Success) 
-                 return result;
+                if (!result.Success) 
+                    return BadRequest(result);
              
-             var fullUser = (User)result.Data!;
+                var fullUser = (User)result.Data!;
 
-             var token = tokenService.GenerateToken(fullUser);
+                var token = tokenService.GenerateToken(fullUser);
 
-             var user = mapper.Map<User, UserModel>(fullUser);
+                var user = mapper.Map<User, UserModel>(fullUser);
              
-             result.Data = new {user, token} ;
+                result.Data = new {user, token} ;
 
-             return result;
+                return Ok(result);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, new GenericCommandResult(false, "Internal server error!", null, null));
+            }
         }
         
         [HttpPost]
         [Route("login")]
-        public async Task<GenericCommandResult> Login(
+        public async Task<IActionResult> Login(
             [FromBody]LoginUserModel model,
             [FromServices]IUserRepository repository,
             [FromServices]TokenService tokenService)
         {
             if (!ModelState.IsValid)
-                return new GenericCommandResult(false, 
+                return BadRequest(new GenericCommandResult(false, 
                     "Oops, looks like that lack anything!",
                     null,
                     ModelState.Select(x => 
-                    new{x.Key, message = x.Value!.Errors[0].ErrorMessage}));
+                        new{x.Key, message = x.Value!.Errors[0].ErrorMessage})));
             
             try
             {
                 var user = await repository.GetByEmail(model.Email);
                 if (user == null || !PasswordHasher.Verify(user.Password, model.Password))
-                    return new GenericCommandResult(false,
+                    return StatusCode(401,  new GenericCommandResult(false,
                         "Your email or password is incorrect!",
                         null,
-                        null);
+                        null));
                 
                 var token = tokenService.GenerateToken(user);
                 
-                return new GenericCommandResult(false, "Logged In!", token,null);
+                return Ok(new GenericCommandResult(true, "Logged In!", new {token},null));
             }
             catch (Exception e)
             {
-                Console.WriteLine(e.Message);
-                throw;
+                return StatusCode(500, new GenericCommandResult(false, "Internal server error!", null, null));
             }
             
             
