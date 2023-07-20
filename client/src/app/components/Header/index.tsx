e4baceddb35b01f95ea34b85697d33b0c8689609
Italo Covas <italocovas@gmail.com>
2023-07-20 09:22:39 -0300

    feature: dark mode

--- client/src/app/components/Header/index.tsx
+++ client/src/app/components/Header/index.tsx
@@ -1,17 +1,26 @@
 import Image from 'next/image';
+import { FiMoon, FiSun } from 'react-icons/fi';
 
 import shopflixLogo from '../../../assets/images/shopflix.png';
+import useThemeSwitcher from '@/app/utils/hooks/useThemeSwitcher';
 
 export default function Header(): JSX.Element {
+  const [theme, setTheme] = useThemeSwitcher();
+
+  function handleToggleTheme(): void {
+    setTheme((previousTheme) => (previousTheme === 'dark' ? 'light' : 'dark'));
+  }
+
   return (
     <header className="flex items-center h-20 justify-between flex-wrap bg-teal-500 p-6 sm:flex-col sm:h-fit">
       <Image src={shopflixLogo} width={130} height={100} alt="Shopflix Logo" />
       <div className="text-xl flex items-center  sm:flex-col sm:mt-5 lg:flex-row">
-        <a
-          href="#responsive-header"
-          className="block sm:mt-4 lg:inline-block mt-0 text-teal-200 hover:text-white mr-12 sm:mr-0"
-        >
-          ThemeS
+        <a className="block sm:mt-4 lg:inline-block mt-0 text-teal-200 hover:text-white mr-12 sm:mr-0 hover:cursor-pointer">
+          {theme === 'dark' ? (
+            <FiMoon size={25} onClick={handleToggleTheme} />
+          ) : (
+            <FiSun size={25} onClick={handleToggleTheme} />
+          )}
         </a>
         {/* <a
           href="#responsive-header"
@@ -19,11 +28,8 @@ export default function Header(): JSX.Element {
         >
           TODO: Recycled Paper
         </a> */}
-        <a
-          href="#responsive-header"
-          className="block sm:mt-4 lg:inline-block mt-0 text-teal-200 hover:text-white sm:mr-0"
-        >
-          Sobre
+        <a className="block sm:mt-4 lg:inline-block mt-0 text-teal-200 hover:text-white sm:mr-0 select-non">
+          Limpar lista
         </a>
       </div>
     </header>
--- client/src/app/shoplist/page.tsx
+++ client/src/app/shoplist/page.tsx
@@ -1,32 +1,59 @@
 'use client';
-import { useEffect, useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import Header from '../components/Header';
+import '../input.css';
+
+interface ITestList {
+  text: string;
+  checked: boolean;
+}
 
 export default function ShopList(): JSX.Element {
-  const [testList, setTestList] = useState<any>([]);
+  const [testList, setTestList] = useState<ITestList[]>([]);
+  const inputRef = useRef<HTMLInputElement>(null);
 
   useEffect(() => {
     setTestList([
       {
-        text: 'Eat Breakfeastkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk'
-      },
-      {
-        text: 'Feed Pugsly, the coasdasdsadasdsadw'
-      },
-      {
-        text: 'Prepare Dinner'
-      },
-      {
-        text: 'Join a hangout in google+'
-      },
-      {
-        text: 'Call mom'
-      },
-      {
-        text: 'Tweet about feeding my cow, pugsly'
+        text: '',
+        checked: false
       }
     ]);
   }, []);
+
+  function handleCheckedChange(idx: number): void {
+    setTestList((prevTestList) =>
+      prevTestList.map((item, index) =>
+        index === idx ? { ...item, checked: !item.checked } : item
+      )
+    );
+  }
+
+  function handleAddItem(
+    idx: number,
+    event: React.KeyboardEvent<HTMLInputElement>
+  ): void {
+    if (event.key === 'Enter') {
+      const inputValue = inputRef.current?.value ?? '';
+      if (inputValue.trim() !== '') {
+        setTestList((prevTestList) =>
+          prevTestList.map((item, index) =>
+            index === idx ? { checked: false, text: inputValue } : item
+          )
+        );
+
+        setTestList((prevTestList) => [
+          ...prevTestList,
+          { checked: false, text: '' }
+        ]);
+
+        if (inputRef.current !== null) {
+          inputRef.current.value = '';
+        }
+        // Clear the input value after adding the new item
+      }
+    }
+  }
 
   return (
     <div className="h-screen leading-[30px]">
@@ -37,18 +64,45 @@ export default function ShopList(): JSX.Element {
           height: 'calc(100vh - 80px)'
         }}
       >
-        <div className="relative w-[90%] max-w-[800px] h-[400px] bg-neutral-50 shadow-[0_2px_8px_rgba(0,0,0,0.3)] overflow-hidden mx-auto my-0 rounded-[10px] before:content-[''] before:absolute before:w-[60px] before:bg-gradient-radial before:bg-repeat-y before:bg-[length:30px_30px] before:box-border before:border-r-[3px] before:border-r-[#D44147] before:border-solid before:left-0 before:inset-y-0">
-          <div className="absolute bg-gradient-linear-lines bg-[length:30px_30px] left-[60px] right-0 inset-y-[20px]">
+        <div className="relative flex w-[90%] max-w-[800px] h-[400px] bg-neutral-50 shadow-[0_2px_8px_rgba(0,0,0,0.3)] overflow-hidden mx-auto my-0 rounded-[10px] before:content-[''] before:absolute before:w-[60px] before:bg-gradient-radial before:bg-repeat-y before:bg-[length:30px_30px] before:box-border before:border-r-[3px] before:border-r-[#D44147] before:border-solid before:left-0 before:inset-y-0">
+          <div className="absolute bg-gradient-linear-lines bg-[length:30px_30px] left-[60px] right-0 inset-y-[20px] overflow-y-scroll">
             <ul className="text-[#555] text-[22px]">
-              {testList.map((item: any, idx: number) => {
+              {testList.map((item: ITestList, idx: number) => {
                 return (
-                  <div key={idx} className="flex ml-10 items-start">
-                    <input type="checkbox" className="mt-2" />
+                  <div key={idx} className="flex ml-2 items-start">
                     <li
-                      className="capitalize ml-4 hover:bg-gray-200 transition-all duration-200 hover:cursor-pointer break-all inline-flex"
+                      className={`capitalize ml-4 w-full transition-all duration-200 hover:cursor-pointer break-all inline-flex `}
+                      onClick={() => {
+                        handleCheckedChange(idx);
+                      }}
                       key={idx}
                     >
-                      <p className="inline">{item.text}</p>
+                      {item.text === '' && (
+                        <input
+                          ref={inputRef}
+                          className="inline select-none border-none outline-none bg-transparent w-full"
+                          onKeyDown={(e) => {
+                            handleAddItem(idx, e);
+                          }}
+                        />
+                      )}
+                      {item.text !== '' && (
+                        <p
+                          className={`inline select-none ${
+                            item.checked ? 'line-through' : ''
+                          }`}
+                        >
+                          {item.text}
+                        </p>
+                      )}
+                      {item.text === '' && (
+                        <span
+                          className="absolute right-3 text-2xl font-bold"
+                          onClick={() => {}}
+                        >
+                          +
+                        </span>
+                      )}
                     </li>
                   </div>
                 );
