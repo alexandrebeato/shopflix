eb59297701a52bd6a14ec36b6597975554a90101
Italo Covas <italocovas@gmail.com>
2023-08-04 00:27:02 -0300

    feat: marking item as purchased

--- client/src/app/shoplist/page.tsx
+++ client/src/app/shoplist/page.tsx
@@ -15,6 +15,11 @@ interface CreateItemProps {
   description: string;
 }
 
+interface PurchaseItemProps {
+  id: string;
+  userId: string;
+}
+
 interface ItemProps {
   userId?: string;
   description: string;
@@ -24,28 +29,29 @@ interface ItemProps {
   createdAt?: string;
 }
 
-const comicNeue = Comic_Neue({ subsets: ['latin'], weight: '400' });
-
-async function getItems(): Promise<ItemProps[] | undefined> {
-  try {
-    const res = await api.get<AxiosResponse>('/items', {});
-
-    return res.data.data;
-  } catch (e) {
-    const error = e as AxiosError;
-    console.log(error);
-  }
+interface ResponseItemProps {
+  data: {
+    userId?: string;
+    description: string;
+    isPurchased: boolean;
+    purchasedAt?: string;
+    id?: string;
+    createdAt?: string;
+  };
 }
+
+const comicNeue = Comic_Neue({ subsets: ['latin'], weight: '400' });
 
 export default function ShopList(): JSX.Element {
   const [shoplist, setShoplist] = useState<ItemProps[]>([]);
+  const [userId, setUserId] = useState<string>('');
   const inputRef = useRef<HTMLInputElement>(null);
-
-  console.log(shoplist);
 
   useEffect(() => {
     (async () => {
-      const items = await getItems();
+      const items = await fetchItems();
+      const userId = await fetchUserId();
+      setUserId(userId);
       setShoplist(items as ItemProps[]);
       setShoplist(
         (prevShoplist: ItemProps[]): ItemProps[] =>
@@ -57,68 +63,113 @@ export default function ShopList(): JSX.Element {
     })();
   }, []);
 
-  function handleCheckedChange(idx: number): void {
-    setShoplist((prevShoplist) =>
-      prevShoplist.map((item, index) =>
-        index === idx ? { ...item, isPurchased: !item.isPurchased } : item
-      )
+  async function fetchItems(): Promise<ItemProps[] | undefined> {
+    try {
+      const res = await api.get<AxiosResponse>('/items', {});
+
+      return res.data.data;
+    } catch (e) {
+      const error = e as AxiosError;
+      console.log(error);
+    }
+  }
+
+  async function fetchUserId(): Promise<string> {
+    const res = await api.get<CookieResponse>(
+      '/api/cookies',
+      {},
+      'http://localhost:3000'
+    );
+    return res.data.userData.userId;
+  }
+
+  async function createItem(
+    userId: string,
+    description: string
+  ): Promise<ItemProps> {
+    const payload = { userId, description };
+    const res = await api.post<CreateItemProps, ResponseItemProps>(
+      payload,
+      '/items'
+    );
+    return res.data.data;
+  }
+
+  function updateShoplist(idx: number, updatedItem: ItemProps): void {
+    setShoplist((prevShoplist: ItemProps[]): ItemProps[] =>
+      prevShoplist.map((item, index) => (index === idx ? updatedItem : item))
+    );
+
+    setShoplist(
+      (prevShoplist: ItemProps[]): ItemProps[] =>
+        [
+          ...prevShoplist,
+          { description: '', isPurchased: 'false' }
+        ] as ItemProps[]
     );
   }
 
   async function handleAddItem(
-    idx: number,
-    event: React.KeyboardEvent<HTMLInputElement>
+    event: React.KeyboardEvent<HTMLInputElement>,
+    idx: number
   ): Promise<void> {
-    if (event.key === 'Enter') {
-      const inputValue = inputRef.current?.value ?? '';
-      if (inputValue.trim() !== '') {
-        try {
-          const cookies = await api.get<CookieResponse>(
-            '/api/cookies',
-            {},
-            'http://localhost:3000'
-          );
+    if (event.key !== 'Enter') {
+      return;
+    }
 
-          const payload = {
-            userId: cookies.data.userData.userId,
-            description: inputValue
-          };
+    const inputValue = inputRef.current?.value?.trim();
+    if (!inputValue) {
+      return;
+    }
 
-          const updatedItem = await api.post<CreateItemProps, any>(
-            payload,
-            '/items'
-          );
+    try {
+      const updatedItem = await createItem(userId, inputValue);
+      updateShoplist(idx, updatedItem);
 
-          console.log(updatedItem);
-
-          // if (inputRef?.current !== null) {
-          //   inputRef.current.value = '';
-          // }
-
-          setShoplist((prevShoplist: ItemProps[]): ItemProps[] =>
-            prevShoplist.map((item, index) =>
-              index === idx ? updatedItem.data.data : item
-            )
-          );
-
-          setShoplist((prevShoplist: ItemProps[]): ItemProps[] => {
-            return [
-              ...prevShoplist,
-              { description: '', isPurchased: 'false' }
-            ] as ItemProps[];
-          });
-
-          toast.success('Item registrado com sucesso!');
-        } catch (e) {
-          const error = e as AxiosError;
-          console.log(error, 'ERRO POST');
-          toast.error(
-            error.response?.status === 403
-              ? 'O nome do item precisa ter mais de 2 caracteres.'
-              : 'Ocorreu um erro ao registrar o item, tente novamente.'
-          );
-        }
+      if (inputRef.current) {
+        inputRef.current.value = '';
       }
+
+      toast.success('Item registrado com sucesso!');
+    } catch (e) {
+      const error = e as AxiosError;
+      toast.error(
+        error.response?.status === 403
+          ? 'O nome do item precisa ter mais de 2 caracteres.'
+          : 'Ocorreu um erro ao registrar o item, tente novamente.'
+      );
+    }
+  }
+
+  async function handleCheckedChange(
+    item: ItemProps,
+    idx: number
+  ): Promise<void> {
+    try {
+      const payload = {
+        id: item.id as string,
+        userId
+      };
+
+      const res = await api.post<PurchaseItemProps, ResponseItemProps>(
+        payload,
+        '/items/purchase'
+      );
+
+      const updatedItem = res.data.data;
+
+      setShoplist((prevShoplist: ItemProps[]): ItemProps[] => {
+        return prevShoplist.map((item, index) =>
+          index === idx ? updatedItem : item
+        );
+      });
+
+      toast.success('Item comprado com sucesso!');
+    } catch (e) {
+      console.log(e);
+      toast.error(
+        'Ocorreu um erro ao marcar o item como comprado, tente novamente.'
+      );
     }
   }
 
@@ -138,12 +189,9 @@ export default function ShopList(): JSX.Element {
             <ul className="text-[#555] text-[22px]">
               {shoplist?.map((item: ItemProps, idx: number) => {
                 return (
-                  <div key={idx} className="flex ml-2 items-start">
+                  <div key={item.id ?? idx} className="flex ml-2 items-start">
                     <li
                       className={`capitalize ml-4 w-full transition-all duration-200 hover:cursor-pointer break-all inline-flex `}
-                      onClick={() => {
-                        handleCheckedChange(idx);
-                      }}
                       key={idx}
                     >
                       {item.description === '' && (
@@ -151,7 +199,7 @@ export default function ShopList(): JSX.Element {
                           ref={inputRef}
                           className="inline select-none border-none outline-none bg-transparent w-full"
                           onKeyDown={async (e) => {
-                            await handleAddItem(idx, e);
+                            await handleAddItem(e, idx);
                           }}
                         />
                       )}
@@ -160,6 +208,9 @@ export default function ShopList(): JSX.Element {
                           className={`inline select-none ${
                             item.isPurchased ? 'line-through' : ''
                           }`}
+                          onClick={() => {
+                            handleCheckedChange(item, idx);
+                          }}
                         >
                           {item.description}
                         </p>
